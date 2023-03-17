5292d2c58252c443558d416fbdcef6c095d4e32a
kairatkubat <83951238+kairatkubat@users.noreply.github.com>
2023-03-17 14:49:37 +0600

    adding third app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:udemy3/screens/product_overview.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData( 
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverVirew(),
+    );
+  }
+}
+
+ 
