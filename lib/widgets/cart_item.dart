b8935d27be82b929ef9d0c42ba088c051a6828ee
kairatkubat <83951238+kairatkubat@users.noreply.github.com>
2023-03-22 17:01:27 +0600

    Making Cart Item dissmisible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
-  const CartItem(this.id, this.price, this.quantity, this.title, {super.key});
+  const CartItem(this.id, this.productId, this.price, this.quantity, this.title, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-
-      margin: const  EdgeInsets.symmetric(horizontal: 15,
-       vertical: 4),
-       child:  Padding(padding: const EdgeInsets.all(8),
-       child: ListTile(
-        leading: CircleAvatar(child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: FittedBox(
-            child: Text('\$$price')),
-        ),
-        ),
-        title: Text(title, style: Theme.of(context).textTheme.titleLarge),
-         subtitle: Text('Total : \$${(price * quantity)}'),
-         trailing: Text('$quantity X'),
-       ),
-       ),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        
+        alignment: Alignment.centerRight,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.error,
+          borderRadius: BorderRadius.circular(5)),
+        child: const Icon(Icons.delete, size: 40, color: Colors.white,),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+           Provider.of<Cart>(context, listen: false).removeItem(productId); 
+      },
+      child: Card(
+    
+        margin: const  EdgeInsets.symmetric(horizontal: 15,
+         vertical: 4),
+         child:  Padding(padding: const EdgeInsets.all(8),
+         child: ListTile(
+          leading: CircleAvatar(child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text('\$$price')),
+          ),
+          ),
+          title: Text(title, style: Theme.of(context).textTheme.titleLarge),
+           subtitle: Text('Total : \$${(price * quantity)}'),
+           trailing: Text('$quantity X'),
+         ),
+         ),
+      ),
     );
   }
 }
